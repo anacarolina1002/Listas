91569d4f0b710bbefeafa6d60664bbb76fa47a27
Anacarolina1002 <carolquaglioto@outlook.com>
2020-09-02 16:19:54 -0300

    :rocket: fix: comandos

--- minhaLista.py
+++ minhaLista.py
@@ -37,4 +37,59 @@ if "Gabriel" in minhaLista:
     print("Sim, está!")
 else:
     print("Não está!")
-    
+
+#Verificar quantos itens tem na lista
+print("Quantidade de itens na lista: ",len(minhaLista))
+
+#Adiciona item ao final da lista
+minhaLista.append("Henrique")
+print("minha lista com um novo item no final: ",minhaLista)
+
+#Adiciona item na posição escolhida
+minhaLista.insert(2, "Madu")
+print("minha lista com item em posição específica: ", minhaLista)
+
+#Remover item específico
+minhaLista.remove("Francine")
+print("minha lista com item removido: ",minhaLista)
+
+#Remove o índice especificado
+minhaLista.pop(4)
+print("minha lista com índice removido: ",minhaLista)
+
+#Remove o útlimo índice da lista
+minhaLista.pop()
+print("minha lista com índice removido: ",minhaLista)
+
+#Atua da mesma forma que o pop
+del minhaLista[1]
+print("Minha lista com item removido com o del: ", minhaLista)
+
+#del pode apagar toda a lista.
+minhaLista.clear()
+print("Minha lista vazia com o método clear: ",minhaLista)
+
+minhaLista.insert(0,"hello")
+minhaLista.insert(1, "opa")
+print("Minha lista com novos itens:", minhaLista)
+
+minhaSegundaLista = minhaLista.copy()
+print("Lista que copiou a primeira:", minhaSegundaLista)
+
+minhaTerceiraLista = list(minhaSegundaLista)
+print("Lista que copiou a segunda:", minhaTerceiraLista)
+
+minhaSegundaLista.append("Aleatório")
+minhaTerceiraLista.insert(0,"Hello")
+
+print("Adicionando novos itens:",minhaSegundaLista)
+print("Adicionando novos itens:",minhaTerceiraLista)
+
+print("Quantidade de vezes que repete o hello: ",minhaTerceiraLista.count("Hello"))
+
+#Juntando listas
+minhaQuartaLista = minhaSegundaLista + minhaTerceiraLista
+print("Juntando listas: ", minhaQuartaLista)
+
+#Encontrando a posição de um item na lista
+print("Encontrando a posição de um item na lista: ", minhaQuartaLista.index("hello"))
